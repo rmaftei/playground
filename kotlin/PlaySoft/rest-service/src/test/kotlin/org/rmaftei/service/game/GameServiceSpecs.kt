4c1453ec65ee566fd9a1fd2294459f15d851144f
Romeo Maftei <mafteiromeo@gmail.com>
2016-05-13 16:39:24 +0300

    Added specs on service

--- kotlin/PlaySoft/rest-service/src/test/kotlin/org/rmaftei/service/game/GameServiceSpecs.kt
+++ kotlin/PlaySoft/rest-service/src/test/kotlin/org/rmaftei/service/game/GameServiceSpecs.kt
@@ -1,35 +1,54 @@
 package org.rmaftei.service.game
 
+import com.google.gson.GsonBuilder
+import com.mashape.unirest.http.Unirest
 import org.jetbrains.spek.api.Spek
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.rmaftei.service.json.GameDeserializer
+import org.rmaftei.service.json.GameSerializer
+import org.rmaftei.service.model.game.Game
+import spark.Spark
+import kotlin.test.assertTrue
 
 class GameServiceSpecs : Spek() {
-//    init {
-//        val gameRepository = EasyMock.createMock(GameRepository::class.java)
-//
-//        val id = "id "
-//        val startTime = DateTime.now()
-//        val location = "location"
-//        val description = "description"
-//        val createdBy = "user"
-//
-//        EasyMock.expect(gameRepository.getAllGames())
-//                .andReturn(listOf(
-//                        Game(id + "1", startTime, location, description, createdBy),
-//                        Game(id + "2", startTime, location, description, createdBy),
-//                        Game(id + "3", startTime, location, description, createdBy))).times(1)
-//
-//        EasyMock.replay(gameRepository)
-//
-//        given("A game service") {
-//            val gameService = SimpleGameService(gameRepository)
-//
-//            on("getting all the games") {
-//                val games = gameService.getAllGames()
-//
-//                it("it should be 3 games") {
-//                    assertEquals(games.size, 3)
-//                }
-//            }
-//        }
-//    }
+
+    val gson = GsonBuilder()
+            .registerTypeAdapter(Game::class.java, GameSerializer())
+            .registerTypeAdapter(Game::class.java, GameDeserializer())
+            .create()
+
+    companion object {
+        @BeforeClass
+        fun beforeAllTests() {
+            org.rmaftei.service.main(emptyArray())
+        }
+
+        @AfterClass
+        fun afterAllTests() {
+            Spark.stop()
+        }
+    }
+
+    init {
+        given("A game api") {
+            val PORT = "4567"
+            val SERVICE_VERSION = org.rmaftei.service.SERVICE_VERSION
+            val RESOURCE = org.rmaftei.service.GAME_PATH
+
+            val URL = "http://localhost:$PORT/$SERVICE_VERSION/$RESOURCE"
+
+
+            on("Getting all the games") {
+
+                val response = Unirest.get(URL)
+
+                assertTrue(response.asJson().status === 200)
+
+                val games = gson.fromJson(response.asJson().body.toString(), mutableListOf<Game>().javaClass)
+
+                assertTrue(games.size === 3)
+            }
+        }
+    }
 }
